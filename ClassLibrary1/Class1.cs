df06fbc349cf4b710f10a5bc45d7ecc2daa39b96
Даниил <kourin.daniil@gmail.com>
2022-04-18 09:54:29 +0300

    exz

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ekzamen;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static Class1 class1;
+        public static Class1 GetClass1()
+        {
+            return class1;
+        }
+        private static void CreateTaskList(string name)
+        {
+            private static string name;
+        private static List<Task> tasks; }
+    } 
+}
